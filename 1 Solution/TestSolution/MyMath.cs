4600659d61ef500ca55b917aa2dab5463eabaeac
Nevio.Weishaupt <nevio.weishaupt@ksb-sg.ch>
2025-08-18 12:25:56 +0200

    ggt und kgv

---
+++ 1 Solution/TestSolution/MyMath.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M320
+{
+    internal class my_math
+    {
+        static internal int Calc_ggT(int a, int b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+            else
+            {
+                return Calc_ggT(b, a % b);
+            }
+        }
+
+        static internal int Calc_kgV(int a, int b)
+        {
+            if (b != 0)
+            {
+                return a % b;
+            }
+            else
+            {
+                return a;
+            }
+        }
+
+        public static int ReadInt()
+        {
+            int zahl;
+            while (true)
+            {
+                Console.Write("Bitte positive Zahl eingeben: ");
+                if (int.TryParse(Console.ReadLine(), out zahl) && zahl > 0)
+                {
+                    return zahl;
+                }
+                Console.WriteLine("Ungültig! Zahl muss > 0 sein.");
+            }
+        }
+    }
+}
---
+++ 1 Solution/TestSolution/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace M320
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 30;
+            int b = 60;
+            int ggT = my_math.Calc_ggT(a, b);
+            int kgV = my_math.Calc_kgV(a, b);
+            Console.WriteLine($"ggT von {a} und {b} = {ggT}");
+            Console.WriteLine($"kgv von {a} und {b} = {kgV}");
+        }
+        static int ReadInt()
+        {
+            int zahl = 0;
+            while (true)
+            {
+                Console.Write("Bitte geben Sie eine Positive Zahl ein.");
+                if (int.TryParse(Console.ReadLine(), out zahl) && zahl > 0)
+                {
+                    return zahl;
+                }
+                Console.WriteLine("Ungültige Zahl.");
+            }
+        }
+    }
+}
+
