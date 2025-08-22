c0b8e27454ebb15da7041a8349b1b17a6780eaab
Nevio.Weishaupt <nevio.weishaupt@ksb-sg.ch>
2025-08-22 21:26:38 +0200

    ggt function

--- mymath/M320/Program.cs
+++ mymath/M320/Program.cs
@@ -4,11 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
             int a = 2;
             int b = 3;
-            int summe = my_math.Add (a, b);
+            int summe = my_math.Add(a, b);
             Console.WriteLine($"Summe von {a} und {b} = {summe}");
         }
     }
-}
+
+
+
+    internal class Program1
+    {
+            static void Main(string[] args)
+            {
+                int a = 2;
+                int b = 3;
+
+                int ggt = my_math.Calc_ggt(a, b);
+                Console.WriteLine($"Der ggT von {a} und {b} ist: {ggt}");
+            }
+        }
+    }
+
+
+
+   
+
--- mymath/M320/my_math.cs
+++ mymath/M320/my_math.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
 
 namespace M320
 {
@@ -10,7 +11,21 @@ namespace M320
     {
         static internal int Add(int a, int b)
         { 
-            return a + b *b; 
+            return a + b; 
+        }
+
+        static internal int Calc_ggt(int a, int b)
+        {
+
+            while (b != 0)
+            {
+                int z = a % b;
+                a = b;
+                b = z;
+
+            }
+            return a;
         }
     }
 }
+
